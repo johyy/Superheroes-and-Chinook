953dffae8329d87bba07561323e2fabdd6e847c9
Sami Sihvonen <sami.sihvonen@fi.experis.com>
2022-08-23 12:36:14 +0300

    merged main

--- Chinook/src/main/java/com/example/Chinook/ChinookApplication.java
+++ Chinook/src/main/java/com/example/Chinook/ChinookApplication.java
@@ -1,11 +1,5 @@
 package com.example.Chinook;
 
-import com.example.Chinook.dao.ChinookDao;
-import com.example.Chinook.models.Customer;
-import com.example.Chinook.runner.ChinookAppRunner;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
